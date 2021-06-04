52330d923fca7dfd3dc0038530e2bf50afeb8e2d
Edgar Krasikov <edhar97@gmail.com>
2021-06-04 19:10:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+
 let popup = document.querySelector(".popup");
 let flash = document.querySelectorAll(".open-window");
 let close = document.querySelector(".popup_close");
 
+//when you press the button 'flashcards' a window pops up
 flash[0].onclick = function () {
     popup.style.display = "block";
 };
+//when you press the button 'virtual piano' a window pops up
 flash[1].onclick = function () {
     popup.style.display = "block";
 }
+
+//when you press the button the window closes
 close.onclick = function () {
     popup.style.display = "none";
 };
